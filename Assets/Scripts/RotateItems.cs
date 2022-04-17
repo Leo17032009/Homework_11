1a796aba8ed1bf38000f4a832af051c2d79bbac7
Leo17032009 <89399852+Leo17032009@users.noreply.github.com>
2022-04-17 07:59:59 +0300

    init

---
+++ Assets/Scripts/RotateItems.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItems : MonoBehaviour 
+{
+	private int _degreesRotation = 60;
+
+	private void Update()
+	{
+		transform.Rotate (0, _degreesRotation * Time.deltaTime, 0);
+	}
+}
